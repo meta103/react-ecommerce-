33df4d663ca6d9d93522e033b47f5265693fb6ea
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-04-06 21:42:24 +0200

    details setup 2

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,9 +6,11 @@ import { ButtonContainer } from './Button';
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from details</h3>
-      </div>
+      <ProductConsumer>
+        {}
+      </ProductConsumer>
     )
   }
 }
+
+
