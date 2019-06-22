5dda6541a2f629cc6d5eae4d74352e37a237507b
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-06-22 13:56:20 +0200

    modal & cart component

--- src/context.js
+++ src/context.js
@@ -9,7 +9,13 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
   state = {
     products: [],
-    detail: detailProduct
+    detailProduct: detailProduct,
+    cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   }
 
   componentDidMount = () => {
@@ -26,18 +32,61 @@ class ProductProvider extends Component {
     this.setState({
       products: products
     });
+  };
+
+  getItem = (id) => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
   }
 
-  handleDetail = () => {
-    console.log("Hello from detail");
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product }
+    })
+  };
 
+  addToCart = (id) => {
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(() => {
+      return { products: tempProducts, cart: [...this.state.cart, product] };
+    }, () => console.log(this.state))
+  };
+
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true }
+    })
   }
 
-  addToCart = () => {
-    console.log("Hello from add to cart");
+  closeModal = () => {
+    this.setState(() => {
+      return { modalOpen: false }
+    })
   }
 
+  increment = (id) => {
+    console.log("increment")
+  }
 
+  decrement = (id) => {
+    console.log("decrement")
+  }
+
+  removeItem = (id) => {
+    console.log("remove item")
+  }
+
+  clearCart = () => {
+    console.log("cart was cleared")
+  }
 
   render() {
     return (
@@ -45,7 +94,13 @@ class ProductProvider extends Component {
         //Esto es desestructuracion que nos retorna todo lo que hay en el state:
         ...this.state,
         handleDetail: this.handleDetail,
-        addToCart: this.addToCart
+        addToCart: this.addToCart,
+        openModal: this.openModal,
+        closeModal: this.closeModal,
+        increment: this.increment,
+        decrement: this.decrement,
+        removeItem: this.removeItem,
+        clearCart: this.clearCart
       }}>
         {this.props.children}
       </ProductContext.Provider>
